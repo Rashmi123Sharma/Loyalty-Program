7b985477d2c52a91c5a68094965fd1061bd3a599
Rashmi123Sharma <sharmarashmisml@gmail.com>
2023-05-02 12:34:33 +0530

    model added

--- api/models.py
+++ api/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+
 # Create your models here.
+class UserDetails(models.Model):
+    username=models.CharField(max_length=100,unique=True)
+    firstname=models.CharField(max_length=100,unique=True)
+    lastname=models.CharField(max_length=100,unique=True)
+    age=models.IntegerField(default=20)
+    phone =models.IntegerField(default=20)
+    country=models.CharField(max_length=100,unique=True)
