61f419c830b567cab29ba3d5725e974dee7a9764
mhkpreet87 <mhk.preet77@gmail.com>
2023-05-02 12:43:37 +0530

    chnages done

--- api/models.py
+++ api/models.py
@@ -6,6 +6,6 @@ class UserDetails(models.Model):
     username=models.CharField(max_length=100,unique=True)
     firstname=models.CharField(max_length=100,unique=True)
     lastname=models.CharField(max_length=100,unique=True)
-    age=models.IntegerField(default=20)
+    dob=models.DateField()
     phone =models.IntegerField(default=20)
     country=models.CharField(max_length=100,unique=True)
