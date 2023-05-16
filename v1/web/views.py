fa4dc6782481bcc4f9d6d8ce18b21da7264b1279
Rashmi123Sharma <sharmarashmisml@gmail.com>
2023-05-16 14:17:29 +0530

    Login api

--- v1/web/views.py
+++ v1/web/views.py
@@ -358,30 +358,22 @@ class LoginViewSet(ViewSet):
     def create(self, request):
         identity=request.data.get('identity')
         password=request.data.get('password')
-        username=base64.b64decode(username).decode('ascii')
-        email=base64.b64decode(email).decode('ascii')
-        password=base64.b64decode(password).decode('ascii')
+        # identity=base64.b64decode(identity).decode('ascii')
+        # password=base64.b64decode(password).decode('ascii')
         user=User.objects.filter(Q(username=identity) | Q(email=identity)).first() #return boolean
         if user:
             if user.check_password(password):
                 #password_valid
                 token=AccessToken.for_user(user)
                 token=str(token)
-                dashboard_user = DashboardUser.objects.filter(user=user).first()
+                dashboard_user=DashboardUser.objects.filter(user=user).first()
+                serializers=DashboardUserSerializer(dashboard_user)
+
                 data={
                     'status':True,
                     'message':'login success',
                     'token':token,
-                    'full_name': dashboard_user.full_name,
-                    'brand_name': dashboard_user.brand_name ,
-                    'business_sector': dashboard_user.business_sector,
-                    'store_categories': dashboard_user.store_categories,
-                    'number_of_stores': dashboard_user.number_of_stores ,
-                    'street_address': dashboard_user.street_address,
-                    'state': dashboard_user.state  ,
-                    'city': dashboard_user.city  ,
-                    'pincode': dashboard_user.pincode ,
-                    'profile_step': dashboard_user.profile_step ,
+                    'user_data':serializers.data
                 }
                 return Response(data)
                 
