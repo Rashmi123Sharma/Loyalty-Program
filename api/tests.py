2265fa3ddd42f6819ccf2f695147fde305b57049
Rashmi123Sharma <sharmarashmisml@gmail.com>
2023-05-23 10:23:50 +0530

    LoginTestCase

--- api/tests.py
+++ api/tests.py
@@ -1,31 +1,22 @@
+from django.contrib.auth.models import User
+from django.test import TestCase
+from rest_framework.test import APIClient
 from django.urls import reverse
-from rest_framework.test import APITestCase
-from rest_framework import status
-from rest_framework.viewsets import ViewSet
+from v1.web.views import LoginViewSet
 
-class LoginTestCase(APITestCase):
-    def test_login_success(self):
-        url = reverse('v1/web/login')  # Replace 'login' with the actual name or URL pattern of your login view
+class LoginViewSetTestCase(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.user = User.objects.create_user(username='testuser', email='test@example.com', password='testpass')
+
+    def test_successful_login(self):
+        url = reverse('login-list')  # Replace with the actual URL for the login endpoint
         data = {
-            'identity': 'test1@gmail.com',
-            'password': 'password123',
+            'identity': 'testuser',
+            'password': 'testpass'
         }
-        response = self.client.post(url, data=data, format="json")
-        data = response.json()
-        print(data)
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(data['status'], True)
-
-
-   
-    # def login_fail(self):
-    #     _data={
-    #     'identity':'test1@gmail.com',
-    #     'password':'password123',
-    #       }
-    #     _response=self.client.post('v1/web/login',data=_data,format="json")
-    #     _data=_response.json()
-    #     print(_data)
-    #     self.assertEqual(_data['status'],False)
-
-
+        response = self.client.post(url, data=data, format='json')
+        self.assertEqual(response.data['status'], True)
+        self.assertEqual(response.data['message'], 'login success')
+        self.assertIsNotNone(response.data['token'])
+        self.assertIsNotNone(response.data['user_data'])
